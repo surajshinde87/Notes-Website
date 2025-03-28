808a2e172bb5cb5788196c625b54b473c422d2f6
Suraj Shinde <surajpshinde8767@gmail.com>
2025-03-28 18:31:06 +0530

    code updated

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config({
   })
   const app = express();
   app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://notes-making-website.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
